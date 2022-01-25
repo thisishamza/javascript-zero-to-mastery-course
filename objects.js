a0e4f6281de7f0860147dbd1c06f310de9712b38
thisishamza <f168345@nu.edu.pk>
2022-01-26 00:17:51 +0500

    Added loops for objects

--- objects.js
+++ objects.js
@@ -39,3 +39,26 @@ const key = "email";
 //if you want to insert this key into person object
 person[key] = "test@gmail.com" //if you do this then a email : "test@gmail.com" will be added to person object
 person["key"] = "test@gmail.com" //if you do this then a key : "test@gmail.com" will be added to person object
+
+
+//how to iterate over object
+const person = {
+    name: "harshit",
+    age: 22,
+    hobbies: ["guitar", "sleeping", "listening music"] //keep in mind space between person and hobbies
+}
+
+//for in loop
+for (let key in person){
+    console.log(key); //will print the keys: name , age , hobbies
+    console.log(person.key); //will print undefind (cant use dot notation)
+    console.log(person[key]); //will print the values
+    console.log(`${key} : ${person[key]}`); //will print the values
+}
+
+console.log(Object.keys(person)); //will return all the keys  
+
+//for off loop
+for (let key of object.keys(person)) {
+    console.log(person[key]);
+}

518dacf6b1e780ebe71512c5b36889b47fe23aec
thisishamza <f168345@nu.edu.pk>
2022-01-26 00:43:17 +0500

    added object destructuring

--- objects.js
+++ objects.js
@@ -62,3 +62,77 @@ console.log(Object.keys(person)); //will return all the keys
 for (let key of object.keys(person)) {
     console.log(person[key]);
 }
+
+//computed properties
+const key1= "objkey1";
+const key2= "objkey2";
+
+const value1 = "myValue1";
+const value2 = "myValue2";
+
+const obj={
+    key1: value1,
+    key2: value2
+}
+//this will create an object like key1:myValue1 , key2:myValue2;
+// computation wont happen for keys
+
+const obj={
+    [key1]: value1,
+    [key2]: value2
+}
+//this will create an object like objkey1 : myValue1 , objkey2:myValue2
+
+const obj1 = {
+    key1 : "value1",  //this will not be error even though we have to two same keys
+    key2 : "value2",    // the created obj1 will have key2 : "value2" and key1: "value3" 
+    key1: "value3",    // second assignemt will replace the first one for the key
+};
+
+
+//note for spread operator for arrays
+const myArray = [..."1234"]; // array will elements 1, 2 ,3 ,4
+
+//spread operator in objects
+const obj1 = {
+    key1 : "value1"
+};
+const obj2 = {
+    key2 : "value2"
+};
+
+const newObject = {...obj1};  //clone of obj1
+const newObject = {...obj1,...obj2} //new object with both values
+
+
+const obj1 = {
+    key1 : "value1"
+};
+const obj2 = {
+    key1: "value3",
+    key2 : "value2"
+};
+
+const newObject = {...obj1,...obj2} //new object with both values but key1 from obj2
+
+const newObject = {...obj1,...obj2, key4: "test"} //new pbject with extra key
+
+const newObject = {... "abc"} //new objects with index as key and a as values
+// 0: a , 1: b, 2:c
+
+
+
+// object destructuring
+const band = {
+    bandName: "queens",
+    famousSong: "bohemian ",
+    year: 2000,
+    secondFamousSong: "radio"
+};
+//usual way
+const bandName = band.bandName ;
+const famousSong = band.famousSong; 
+
+//shortcut 
+const{bandName, famousSong} = band;
+const{bandName, famousSong, ...restProps} = band; //for remaining properties

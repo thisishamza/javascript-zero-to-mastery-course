66e967a3c5da792067530112ba725d27693b4724
thisishamza <f168345@nu.edu.pk>
2022-01-26 23:50:07 +0500

    Hoisting added to function js file

--- functions.js
+++ functions.js
@@ -55,3 +55,22 @@ const useThisMessage= message => {
 
 // you can also write arrow function like this
 const useThisMessage= message => console.log(message);
+
+
+//hoisting
+
+hello(); //function can be called before its declaration
+
+function hello() { //function declaration
+    console.log("Hello World")
+}
+// NOTE you can call the function before its declaration
+// if you use function expression then you cannot call the function before 
+
+console.log(hello); 
+//if we used that variable before it is declared then it will behave differently based on 
+// its type whether it is a var , a const or let
+
+var hello = "Hello world"; // undefined
+let hello = "Hello world"; // this will be an error
+const hello = "Hello world"; // error in this case also

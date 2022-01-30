beeffba4e4428b435b0a634f30e612c226d5dde6
thisishamza <f168345@nu.edu.pk>
2022-01-31 03:58:53 +0500

    Added reduce method in array methods

--- array_methods_forEach_reduce_etc.js
+++ array_methods_forEach_reduce_etc.js
@@ -81,3 +81,49 @@ const numbers=[1,2,3,4,5,6,7,8,9,10,11]
 //filter all the even numbers from the array
 const isEven = (number) =>{return number%2===0;};
 const evenNumbers= numbers.filter(isEven);
+
+
+// reduce method
+
+const numbers = [1,2,3,4,5,10]
+//reduce takes in a callback function
+ 
+// sum all elements of a array
+const sum = numbers.reduce((accumulator, cuurentValue) => {
+    return accumulator + cuurentValue;
+});
+/* accumulator takes in the first element of the array and current values contains
+the second value in the first iteration then iteration starts*/
+
+const sum = numbers.reduce((accumulator, cuurentValue) => {
+    return accumulator + cuurentValue;
+},100);
+
+// accumulator , cuurentValue , return
+// 1                 2            3
+// 3                 3            6
+// 6                 4            10
+// 10                5            15
+// 15                10           25
+
+// you can also pass initial value with callback function
+/*passing a default value means that will be value of accumulator
+for the first time and current value will be same (2 in this case)*/ 
+
+
+// real world example for reduce method
+
+const userCart = [
+    {productId:1, productName:"mobile", price:1299},
+    {productId:2, productName:"laptop", price:22000},
+    {productId:3, productName:"pc", price:1000}
+]
+
+const totalAmount = userCart.reduce((totalPrice,currentProduct) =>{
+    return totalPrice + currentProduct.price;
+});
+
+// total price  //currentValue  //return
+// 0                {}              1299
+// 1299             220000          23299
+// 23299            1000            24299

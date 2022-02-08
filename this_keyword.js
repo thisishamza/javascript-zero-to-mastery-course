b1b484bb372a2537d67fd8d386dd37813328a9db
Muhammad Hamza <62892109+thisishamza@users.noreply.github.com>
2022-02-08 16:43:44 +0500

    Add another way to declare method  inside object

--- this_keyword.js
+++ this_keyword.js
@@ -65,112 +65,14 @@ const user1={
     about: function() {
         console.log(this.firstname, this.age);
     },
-    aboutDetailed: function(hobby. favMusician) {
-        console.log(this.firstname, this.age);
-    }
-}
-
-
-const user2={
-    firstname: "hamza",
-    age: 19,
-}
-
-/* we are calling the function of obj user1 
-and passing it the user2 object then, 
-this contains the obj2 , so firstName and age 
-of user2 will be consoled on screen */
-
-user1.about.call(user2);
-
-
-user1.aboutDetailed.call(user2,"guitar","musician");
-aboutDetailedGlobal.call(user2,"guitar","musician");  // this function is not defined inside object
-
-
-// apply is just like call(internally it is using the call method)
-
-user.aboutDetailed.apply(user2,["guitar","mozart"]);
-aboutDetailedGlobal.apply(user2,["guitar","mozart"]);
-
-
-// bind returns a function which can be called later
-const func=user.aboutDetailed.bind(user1,["guitar","mozart"]);
-const func=aboutDetailedGlobal.bind(user1,["guitar","mozart"]);
-
-func();  //bind returned a function 
-
-
-/*
-methods : methods are function inside object
-*/
-
-// this value is determined at runtime
-
-
-function personInfo() { //this function can also be defined inside object
-    console.log(`person name is ${this.firstname}`);
-}
-
-const person = {
-    firstName: "hamza",
-    age: 8,
-    about: personInfo
-}
-
-
-const person1 = {
-    firstName: "hamza",
-    age: 8,
-    about: function () {
-        console.log(`person name is ${this.firstname}`);
-        // this is a object , which this function is calling
-        // person is calling the function so this contains the whole person oject
-    }
-}
-
-person.about();
-// if we store the about function object in a var and then call the function, then this will be pointing toward windows not the object
-test = person.about;
-test(); // undefind because this is pointing to window not
-
-
-// if we just do this
-console.log(this); // then window object will be printed
-
-function myFunction() {
-    "user strict"   // if we use strict mode then undefined will be consoled
-    console.log(this);
-}
-myFunc(); //again window object will be printed
-
-
-
-
-
-// call
-// apply
-// bind
-
-function hello() { 
-    console.log("hello world")
-}
-hello.call(); // call is used to call any function
-
-function aboutDetailedGlobal(hobby, favMusician) {
-    console.log(this.firstname, this.age);
-}
-
-
-const user1={
-    firstname: "hamza",
-    age: 19,
-    about: function() {
+    aboutDetailed: function(hobby, favMusician) {
         console.log(this.firstname, this.age);
     },
-    aboutDetailed: function(hobby. favMusician) {
+    // about detailed method can also be declared like this:
+    aboutDetailed(hobby, favMusician) {  // so this also a method which is inside a object just like the above method
         console.log(this.firstname, this.age);
     }
+
 }
 
 

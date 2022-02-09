00bf880bbb80d80e4f94b7a6a16be0a6f72d857a
Muhammad Hamza <62892109+thisishamza@users.noreply.github.com>
2022-02-09 12:51:03 +0500

    Add user object creation using prototype

--- prototype.js
+++ prototype.js
@@ -32,3 +32,31 @@ hello.prototype.sing = function(){
 };
 console.log(hello.prototype.sing());
 
+
+// Now lets use prototype for user's object creation
+
+function createUser(firstName, lastName, email, age, address){
+    const user = Object.create(createUser.prototype); // linking __proto__ with function prototype
+    user.firstName = firstName;
+    user.lastName = lastName;
+    user.email = email;
+    user.age = age;
+    user.address = address;
+    return user;
+}
+createUser.prototype.about = function(){
+    return `${this.firstName} is ${this.age} years old.`;
+};
+createUser.prototype.is18 = function (){
+    return this.age >= 18; 
+}
+createUser.prototype.sing = function (){
+    return "la la la la ";
+}
+
+
+const user1 = createUser('harshit', 'vashsith', 'harshit@gmail.com', 18, "my address");
+const user2 = createUser('harsh', 'vashsith', 'harshit@gmail.com', 19, "my address");
+const user3 = createUser('mohit', 'vashsitha', 'harshit@gmail.com', 17, "my address");
+console.log(user1);
+console.log(user1.sing());

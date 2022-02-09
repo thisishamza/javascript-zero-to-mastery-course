816ad3ecaaa3bf7328927fb46bafe86a56c32143
thisishamza <f168345@nu.edu.pk>
2022-02-09 13:20:25 +0500

    added object creation with new keyword

--- objects_and_functions_and_memory_issues_03.js
+++ objects_and_functions_and_memory_issues_03.js
@@ -25,6 +25,8 @@ hello.prototype.sing = function(){
 };
 console.log(hello.prototype.sing());
 
+    /*************************************/
+
 
 // now using prototype for object creation with methods
 function createUser(firstName, lastName, email, age, address){
@@ -50,5 +52,41 @@ createUser.prototype.sing = function (){
 const user1 = createUser('harshit', 'vashsith', 'harshit@gmail.com', 18, "my address");
 const user2 = createUser('harsh', 'vashsith', 'harshit@gmail.com', 19, "my address");
 const user3 = createUser('mohit', 'vashsitha', 'harshit@gmail.com', 17, "my address");
+console.log(user1);
+console.log(user1.is18());
+
+    /*************************************/
+
+// new keyword 
+// 1.) this = {}
+// 2.) return {} 
+//
+
+// __proto__ 
+// // official ecmascript document
+// [[prototype]]
+
+// constructor function 
+function CreateUser(firstName, lastName, email, age, address){
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.email = email;
+    this.age = age;
+    this.address = address;
+}
+CreateUser.prototype.about = function(){
+    return `${this.firstName} is ${this.age} years old.`;
+};
+CreateUser.prototype.is18 = function (){
+    return this.age >= 18; 
+}
+CreateUser.prototype.sing = function (){
+    return "la la la la ";
+}
+
+
+const user1 = new CreateUser('harshit', 'vashsith', 'harshit@gmail.com', 18, "my address");
+const user2 = new CreateUser('harsh', 'vashsith', 'harshit@gmail.com', 19, "my address");
+const user3 = new CreateUser('mohit', 'vashsitha', 'harshit@gmail.com', 17, "my address");
 console.log(user1);
 console.log(user1.is18());
